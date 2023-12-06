c31b96538706faf0d2cfa5a174824973538f438a
randomstr1ng <julian@petersohn.it>
2023-12-06 10:31:34 +0000

    update security group

--- s4-hana/network.tf
+++ s4-hana/network.tf
@@ -32,13 +32,95 @@ resource "aws_security_group" "security_group" {
     from_port   = 22
     protocol    = "tcp"
     to_port     = 22
+    description = "SSH access"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 3299
+    from_port   = 3200
     protocol    = "tcp"
-    to_port     = 3299
+    to_port     = 3200
+    description = "SAPGUI Instance 00"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3300
+    protocol    = "tcp"
+    to_port     = 3300
+    description = "RFC Instance 00"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30213
+    protocol    = "tcp"
+    to_port     = 30213
+    description = "SAP HANA MDC Database"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50000
+    protocol    = "tcp"
+    to_port     = 50000
+    description = "AS ABAP HTTP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50001
+    protocol    = "tcp"
+    to_port     = 50001
+    description = "AS ABAP HTTPS"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50013
+    protocol    = "tcp"
+    to_port     = 50013
+    description = "SAP Start Service HTTP Instance 00"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50014
+    protocol    = "tcp"
+    to_port     = 50014
+    description = "SAP Start Service HTTPS Instance 00"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50113
+    protocol    = "tcp"
+    to_port     = 50113
+    description = "SAP Start Service HTTP Instance 01"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50114
+    protocol    = "tcp"
+    to_port     = 50114
+    description = "SAP Start Service HTTPS Instance 01"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50213
+    protocol    = "tcp"
+    to_port     = 50213
+    description = "SAP Start Service HTTP Instance 02"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50214
+    protocol    = "tcp"
+    to_port     = 50214
+    description = "SAP Start Service HTTPS Instance 02"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
